a4a3caff142c48975724b3f8363529fdc14a1fff
Jose-MQ <jmqjgc@gmail.com>
2025-04-10 10:33:47 +0200

    Update errores.sh to fail

--- errores.sh
+++ errores.sh
@@ -3,6 +3,6 @@ DIR=/home/jose/onboarding-devops/sprint2/proyecto-pruebas/images/*
 
 rm -r $DIR
 
-echo "Este script se esta ejecutando para eliminar todo el contenido de la carpeta images del repositorio"
+echo Este script se esta ejecutando para eliminar todo el contenido de la carpeta images del repositorio
 
 date +%d-%m-%y/%H:%M:%S
