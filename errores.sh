fa90f7761031b6c1ac5e41a09e097683a5c25bee
Jose-MQ <jmqjgc@gmail.com>
2025-04-10 10:13:20 +0200

    añadido lineas en script errores.sh

--- errores.sh
+++ errores.sh
@@ -2,3 +2,7 @@
 DIR=/home/jose/onboarding-devops/sprint2/proyecto-pruebas/images/*
 
 rm -r $DIR
+
+echo "Este script se esta ejecutando para eliminar todo el contenido de la carpeta images del repositorio"
+
+date +%d-%m-%y/%H:%M:%S
